cd76162e30e94e1cf891cc4a522aadf323a1ad87
Sasha <147207306+AlexandraB-C@users.noreply.github.com>
2024-10-31 23:41:52 +0200

    Task4: + new txt files + main program handles multiple files now + toString method for better output formatting

---
+++ java-classification/src/main/java/lab1/task4/FileReader.java
@@ -0,0 +1,17 @@
+package lab1.task4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileReader {
+    // read file from known path
+    public String readFileIntoString(String path) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(path)));
+        } catch (IOException e) {
+            System.out.println("error reading file: " + e.getMessage());
+            return "";
+        }
+    }
+}
