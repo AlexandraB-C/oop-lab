dc367ed1994ea46e98868563f39b3d934ba5b33a
Sasha <147207306+AlexandraB-C@users.noreply.github.com>
2024-11-04 22:41:39 +0200

    Task3: + add coffee making methods

---
+++ java-classification/src/main/java/lab2/task3/Coffee.java
@@ -0,0 +1,26 @@
+package lab2.task3;
+
+// basic stuff all coffees need
+public class Coffee {
+    protected Intensity coffeeIntensity;
+    protected final String name;
+
+    public Coffee(Intensity strong) {
+        this.coffeeIntensity = strong;
+        this.name = "Coffee";
+    }
+
+    public enum Intensity {
+        LIGHT, NORMAL, STRONG
+    }
+
+    public void printDetails() {
+        
+        System.out.println("Coffee intensity: " + coffeeIntensity);
+    }
+
+    protected Coffee makeCoffee() {
+        System.out.println("1. Brewing coffee with " + coffeeIntensity + " intensity");
+        return this;
+    }
+}
